9370a41c8cb739a972a1ccc535781eefffb778ea
Dulces123 <jregaladoantonio@gmail.com>
2021-11-09 18:10:16 +0000

    create favorite and contact models

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_09_165101) do
+ActiveRecord::Schema.define(version: 2021_11_09_173801) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "contacts", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "property_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["property_id"], name: "index_contacts_on_property_id"
+    t.index ["user_id"], name: "index_contacts_on_user_id"
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "property_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["property_id"], name: "index_favorites_on_property_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
 
   create_table "properties", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -49,5 +67,9 @@ ActiveRecord::Schema.define(version: 2021_11_09_165101) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "contacts", "properties"
+  add_foreign_key "contacts", "users"
+  add_foreign_key "favorites", "properties"
+  add_foreign_key "favorites", "users"
   add_foreign_key "properties", "users"
 end
