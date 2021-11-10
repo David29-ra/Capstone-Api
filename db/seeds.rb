6dd5ba8d5386f3accf0325c31ac9c4161d630039
Dulces123 <jregaladoantonio@gmail.com>
2021-11-10 05:17:56 +0000

    create fake data for users and properties(without image)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,69 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
 Property.destroy_all
 User.destroy_all
 
-user1 = User.create(email: "dulces123@mail.com", password: "123456", name: 'Dulces', phone: "912900900", role: 0) 
-user2 = User.create(email: "morrito@mail.com", password: "123456", name: 'Morrito', phone: "933900900", role: 1)
-user3 = User.create(email: "moxxito@mail.com", password: "323456", name: 'Morrito', phone: "933960900", role: 1)
+puts "Start seeding! 🐑"
+# Creating Users
+puts 'Start generate users ✨'
+users = User.create([{ email: "test0@mail.com", password: "123456" , name: "Dulces",  phone: "900900901", role: 0},
+            { email: "test1@mail.com", password: "123456" , name: "Morrito", phone: "900900902", role: 1 },
+            { email: "test2@mail.com", password: "123456" , name: "El Traidor", phone: "900900903", role: 1 },
+            { email: "test3@mail.com", password: "123456" , name: "Jose", phone: "900900904", role: 0 },
+            { email: "test4@mail.com", password: "123456" , name: "Martos", phone: "900900905", role: 1 }
+            ]) 
+puts 'End generate users! 😌'
 
-Property.create(user_id: user1.id, operation_type: 0, status: 0, address: "la molina", price: 120, property_type:0, area: 123, description: "asd")
-Property.create(user_id: user2.id, operation_type: 0, status: 0, address: "cercado de lima", price: 120, property_type:0, area: 123, description: "asd", pets: true)
-Property.create(user_id: user2.id, operation_type: 0, status: 0, address: "smp", price: 1, property_type:0, area: 123, description: "asd")
-Property.create(user_id: user3.id, operation_type: 0, status: 0, address: "la victoria", price: 1, property_type:0, area: 123, description: "asdd")
-Property.create(user_id: user3.id, operation_type: 0, status: 0, address: "ate", price: 1, property_type:0, area: 123, description: "asd")
-Property.create(user_id: user3.id, operation_type: 0, status: 0, address: "comas", price: 1, property_type:0, area: 123, description: "asd")
-Property.create(user_id: user2.id, operation_type: 0, status: 0, address: "runrun", price: 1, property_type:0, area: 123, description: "asdas", pets: true)
+puts 'Start generate properties'
+landlords = users.filter{|user| user.role == "landlord"}
+
+landlords.each do |landlord|
+  rand(2..10).times do
+    property_params = {
+      user_id: landlord.id,
+      address: Faker::Address.full_address,
+      price: 10*rand(50..100),
+      area: 1.72*rand(68..100),
+      beds: rand(1..3),
+      baths: rand(1..4),
+      description: Faker::Lorem.paragraph_by_chars(number: 120),
+      status: rand(0..1),
+      property_type: rand(0..1),
+      operation_type: rand(0..1)
+    }
+    property = Property.create(property_params)
+  end
+end
+puts 'End generate properties!'
+
+puts 'Updating to pets allowed properties'
+on_sale_properties = Property.all.filter{|property| property.operation_type == "sale"}
+on_rent_properties = Property.all.filter{|property| property.operation_type == "rent"}
+
+on_sale_properties.each do |property|
+  property.update(pets: true)
+end  
+
+on_rent_properties.each do |property|
+  property.update(pets: [true, false].sample)
+end
+
+puts 'Updating maintenance'
+on_rent_properties.each do |property|
+  property.update(maintenance: 0.1*property.price)
+end
+
+puts 'Creating properties marked as favorites'
+users.each do |user|
+  rand(0..3).times do
+    Favorite.create(user: user, property: Property.all.sample)
+  end
+end
+
+puts 'Creating contacted properties'
+users.each do |user|
+  rand(0..3).times do
+    Contact.create(user: user, property: Property.all.sample)
+  end
+end
+puts 'Seeding finished! 👌'
